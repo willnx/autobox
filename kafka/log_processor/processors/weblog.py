45cf4b69d13f63d87bbb0082a8c3e6d123e21ee8
willnx <willnx84@gmail.com>
2019-04-11 09:51:56 -0700

    created log processor for Apache-style logs

---
+++ kafka/log_processor/processors/weblog.py
@@ -0,0 +1,79 @@
+# -*- coding; UTF-8 -*-
+"""Defines how to process and upload API log file information for vLab analytics"""
+import time
+import ipaddress
+from os import environ
+
+import ujson
+
+from log_processor.worker import LogWorker, InvalidToken
+from log_processor.manager import process_logs
+
+
+class WebLogWorker(LogWorker):
+    """Handles processing the web logs and then uploading data to ElasticSearch"""
+    def process_data(self, data):
+        """Convert the log event into a JSON document, then upload to ElasticSearch"""
+        try:
+            info = self.extract(data)
+        except (ValueError, InvalidToken) as doh:
+            self.log.error('Error: {}, Data: {}'.format(doh, data))
+        else:
+            document = self.format_info(info)
+            self.es.write(document)
+
+    @staticmethod
+    def format_timestamp(timestamp):
+        """Covernt an Apache-style timestamp to one ElasticSearch likes"""
+        # [09/Apr/2019:16:34:39
+        pattern = '[%m/%b/%Y:%H:%M:%S'
+        time_struct = time.strptime(timestamp, pattern)
+        # Formatting the timestmap like this, instead of an EPOCH, means that
+        # ElasticSearch will infer the correct type as "date"
+        # which Grafana depends on
+        formatted = time.strftime('%Y/%m/%d %H:%M:%S', time_struct)
+        return formatted
+
+    def format_info(self, info):
+        """Extract the handy bits of data into a JSON document"""
+        # The logs should adhere to standard Apache web log format
+        # 10.200.217.90 - unset [08/Apr/2019:22:21:57 -0000] "GET /api/1/inf/onefs/task/2b311e03-455c-4409-b8c7-425961533a44? HTTP/1.1" 200 248 "None" "vLab CLI 2019.03.28 rid=85c1c19d38e0485da38d4d0a9da2f43f"
+        # The point of the "source" tag is to handle
+        document = {'source' : info['name'],
+                    'timestamp' : None,
+                    'user' : None,
+                    'client_ip' : None,
+                    'method' : None,
+                    'url' : None,
+                    'status_code' : None,
+                    'user_agent' : None,
+                    'transaction_id': None,
+                    'log' : info['log']}
+        raw = info['log'].split()
+        by_quotes = info['log'].split('"')
+        try:
+             ipaddress.ip_address(raw[0])
+        except ValueError:
+            # must be a traceback, or some other log
+            pass
+        else:
+            document['timestamp'] = self.format_timestamp(raw[3])
+            document['user'] = raw[2]
+            document['client_ip'] = raw[0]
+            document['method'] = raw[5].replace('"', '')
+            document['url'] = raw[6]
+            document['status_code'] = raw[8]
+            # the vLab CLI overloads the User Agent with a transaction id
+            document['user_agent'] = by_quotes[5].split('=')[0].replace('rid', '')
+            try:
+                document['transaction_id'] = by_quotes[5].split('=')[1]
+            except IndexError:
+                pass
+        return ujson.dumps(document)
+
+if __name__ == '__main__':
+    process_logs(worker_cls=WebLogWorker,
+                 topic='web',
+                 server=environ['KAFKA_SERVER'],
+                 work_group='web',
+                 name='WebLogProcessor')
---
+++ kafka/tests/processors/test_weblog.py
@@ -0,0 +1,146 @@
+# -*- coding: UTF-8 -*-
+"""A suite of unit tests for the ``processors.weblog`` module"""
+import unittest
+from unittest.mock import patch, MagicMock
+import os
+import builtins
+
+from log_processor import worker
+from log_processor.processors import weblog
+
+
+class TestWebLogWorker(unittest.TestCase):
+    """A suite of test cases for the ``WebLogWorker`` object"""
+    @classmethod
+    def setUp(cls):
+        """Runs before every test case"""
+        os.environ['CIPHER_KEY_FILE'] = './.fake_file.txt'
+        os.environ['ELASTICSEARCH_SERVER'] = '127.0.0.1'
+        os.environ['ELASTICSEARCH_USER'] = 'bob'
+        os.environ['ELASTICSEARCH_DOC_TYPE'] = 'someLogType'
+        os.environ['ELASTICSEARCH_PASSWD_FILE'] = './.fake_file.txt'
+
+    @classmethod
+    def tearDown(cls):
+        """Runs after every test case"""
+        os.environ.pop('CIPHER_KEY_FILE', None)
+        os.environ.pop('ELASTICSEARCH_SERVER', None)
+        os.environ.pop('ELASTICSEARCH_USER', None)
+        os.environ.pop('ELASTICSEARCH_DOC_TYPE', None)
+        os.environ.pop('ELASTICSEARCH_PASSWD_FILE', None)
+
+    @patch.object(worker, 'ElasticSearch')
+    @patch.object(worker, 'Fernet')
+    @patch.object(builtins, "open")
+    def test_init(self, fake_open, fake_Fernet, fake_ElasticSearch):
+        """``WebLogWorker`` accepts standard ``Worker`` init params"""
+        work_group = 'web'
+        work_queue = MagicMock()
+        idle_queue = MagicMock()
+
+        web_worker = weblog.WebLogWorker(work_group, work_queue, idle_queue)
+
+        self.assertTrue(isinstance(web_worker, worker.Worker))
+
+    @patch.object(worker, 'ElasticSearch')
+    @patch.object(worker, 'Fernet')
+    @patch.object(builtins, "open")
+    def test_format_info(self, fake_open, fake_Fernet, fake_ElasticSearch):
+        """``WebLogWorker`` the 'format_info' method returns JSON"""
+        work_group = 'web'
+        work_queue = MagicMock()
+        idle_queue = MagicMock()
+
+        web_worker = weblog.WebLogWorker(work_group, work_queue, idle_queue)
+        example_info = {'name' : 'some container',
+                        'log' : '10.200.217.90 - unset [08/Apr/2019:22:21:57 -0000] "GET /api/1/inf/onefs/task/2b311e03-455c-4409-b8c7-425961533a44? HTTP/1.1" 200 248 "None" "vLab CLI 2019.03.28 rid=85c1c19d38e0485da38d4d0a9da2f43f"'
+                       }
+        answer = web_worker.format_info(example_info)
+        expected = '{"source":"some container","timestamp":"2019\\/04\\/01 22:21:57","user":"unset","client_ip":"10.200.217.90","method":"GET","url":"\\/api\\/1\\/inf\\/onefs\\/task\\/2b311e03-455c-4409-b8c7-425961533a44?","status_code":"200","user_agent":"vLab CLI 2019.03.28 ","transaction_id":"85c1c19d38e0485da38d4d0a9da2f43f","log":"10.200.217.90 - unset [08\\/Apr\\/2019:22:21:57 -0000] \\"GET \\/api\\/1\\/inf\\/onefs\\/task\\/2b311e03-455c-4409-b8c7-425961533a44? HTTP\\/1.1\\" 200 248 \\"None\\" \\"vLab CLI 2019.03.28 rid=85c1c19d38e0485da38d4d0a9da2f43f\\""}'
+
+        self.assertEqual(answer, expected)
+
+    @patch.object(worker, 'ElasticSearch')
+    @patch.object(worker, 'Fernet')
+    @patch.object(builtins, "open")
+    def test_format_info_traceback(self, fake_open, fake_Fernet, fake_ElasticSearch):
+        """``WebLogWorker`` the 'format_info' handles non-Apache style logs too"""
+        work_group = 'web'
+        work_queue = MagicMock()
+        idle_queue = MagicMock()
+
+        web_worker = weblog.WebLogWorker(work_group, work_queue, idle_queue)
+        example_info = {'name' : 'some container',
+                        'log' : 'Traceback (most recent call last):\n  File "some file", line 42 in test\n'
+                       }
+        answer = web_worker.format_info(example_info)
+        expected = '{"source":"some container","timestamp":null,"user":null,"client_ip":null,"method":null,"url":null,"status_code":null,"user_agent":null,"transaction_id":null,"log":"Traceback (most recent call last):\\n  File \\"some file\\", line 42 in test\\n"}'
+
+        self.assertEqual(answer, expected)
+
+    @patch.object(worker, 'ElasticSearch')
+    @patch.object(worker, 'Fernet')
+    @patch.object(builtins, "open")
+    def test_format_info_other_client(self, fake_open, fake_Fernet, fake_ElasticSearch):
+        """``WebLogWorker`` the 'format_info' method handles user agents not overloaded with a transaction id"""
+        work_group = 'web'
+        work_queue = MagicMock()
+        idle_queue = MagicMock()
+
+        web_worker = weblog.WebLogWorker(work_group, work_queue, idle_queue)
+        example_info = {'name' : 'some container',
+                        'log' : '10.200.217.90 - unset [08/Apr/2019:22:21:57 -0000] "GET /api/1/inf/onefs/task/2b311e03-455c-4409-b8c7-425961533a44? HTTP/1.1" 200 248 "None" "python/requests"'
+                       }
+        answer = web_worker.format_info(example_info)
+        expected = '{"source":"some container","timestamp":"2019\\/04\\/01 22:21:57","user":"unset","client_ip":"10.200.217.90","method":"GET","url":"\\/api\\/1\\/inf\\/onefs\\/task\\/2b311e03-455c-4409-b8c7-425961533a44?","status_code":"200","user_agent":"python\\/requests","transaction_id":null,"log":"10.200.217.90 - unset [08\\/Apr\\/2019:22:21:57 -0000] \\"GET \\/api\\/1\\/inf\\/onefs\\/task\\/2b311e03-455c-4409-b8c7-425961533a44? HTTP\\/1.1\\" 200 248 \\"None\\" \\"python\\/requests\\""}'
+
+        self.assertEqual(answer, expected)
+
+    @patch.object(worker, 'ElasticSearch')
+    @patch.object(worker, 'Fernet')
+    @patch.object(builtins, "open")
+    def test_process_data(self, fake_open, fake_Fernet, fake_ElasticSearch):
+        """``WebLogworker`` 'process_data' formats logs, then uploads to ElasticSearch"""
+        work_group = 'web'
+        work_queue = MagicMock()
+        idle_queue = MagicMock()
+        fake_es = MagicMock()
+        fake_ElasticSearch.return_value = fake_es
+        fake_cipher = MagicMock()
+        fake_cipher.decrypt.return_value =  '{"name":"some container","log":"10.200.217.90 - unset [08\\/Apr\\/2019:22:21:57 -0000] \\"GET \\/api\\/1\\/inf\\/onefs\\/task\\/2b311e03-455c-4409-b8c7-425961533a44? HTTP\\/1.1\\" 200 248 \\"None\\" \\"vLab CLI 2019.03.28 rid=85c1c19d38e0485da38d4d0a9da2f43f\\""}'
+        fake_Fernet.return_value = fake_cipher
+
+        web_worker = weblog.WebLogWorker(work_group, work_queue, idle_queue)
+
+        web_worker.process_data('some encrypted data')
+
+        self.assertTrue(fake_es.write.called)
+
+    @patch.object(worker, 'ElasticSearch')
+    @patch.object(worker, 'Fernet')
+    @patch.object(builtins, "open")
+    def test_process_data_error(self, fake_open, fake_Fernet, fake_ElasticSearch):
+        """``WebLogworker`` 'process_data' logs if it cannot decrypt/de-serialize the data"""
+        work_group = 'web'
+        work_queue = MagicMock()
+        idle_queue = MagicMock()
+        fake_es = MagicMock()
+        fake_ElasticSearch.return_value = fake_es
+        fake_cipher = MagicMock()
+        fake_cipher.decrypt.return_value =  '{Invalid JSON'
+        fake_Fernet.return_value = fake_cipher
+        fake_log = MagicMock()
+
+        web_worker = weblog.WebLogWorker(work_group, work_queue, idle_queue)
+        web_worker.log = fake_log
+
+        web_worker.process_data('some encrypted data')
+        the_args, _ = fake_log.error.call_args
+        error_msg = the_args[0]
+        expected = 'Error: Expected object or value, Data: some encrypted data'
+
+        self.assertEqual(error_msg, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
